d82dde2d157b1f96a578a00b520060eeb0efe67d
MichaelRPaquette <grandtimegames@gmail.com>
2025-02-02 00:53:19 -0700

    add smoothign to camera and add player sprite

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var SPEED: float = 400.0
+@export var ACCELERATION: float = 1500.0
+@export var FRICTION: float = 1000.0
 
-func get_input():
+func get_input(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-
+	if input_direction != Vector2.ZERO:
+		velocity = velocity.move_toward(input_direction * SPEED, ACCELERATION * delta)
+	else:
+		# Apply friction when no input
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	move_and_slide()
